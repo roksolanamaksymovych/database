ddb180897e61a134535360d5d1e1450634c44adc
roksolanamaksymovych <151615831+roksolanamaksymovych@users.noreply.github.com>
2024-12-03 16:10:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from my_project.db_init import db
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Gamivka1505@localhost/database_lab1_eer'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Maks_mia3@localhost/database_lab1_eer'
 
     db.init_app(app)
 
